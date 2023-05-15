3be8ef5147852aa80217c23489678a664e506188
hibix-laptop <hibix.festiwal.com>
2023-05-15 11:50:57 +0200

    Black on files

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from imutils import face_utils
 from matplotlib import pyplot as plt
 from scipy.spatial import distance as dist
 
-#Global Configuration Variables
-FACIAL_LANDMARK_PREDICTOR = "shape_predictor_68_face_landmarks.dat"  # path to dlib's pre-trained facial landmark predictor
-MINIMUM_EAR = 0.22   # Minimum EAR for both the eyes to mark the eyes as open
-MAXIMUM_FRAME_COUNT = 1  # Maximum number of consecutive frames in which EAR can remain less than MINIMUM_EAR, otherwise alert drowsiness
+# Global Configuration Variables
+FACIAL_LANDMARK_PREDICTOR = "shape_predictor_68_face_landmarks.dat"
+MINIMUM_EAR = 0.22
+MAXIMUM_FRAME_COUNT = 1
 eye_counter = 0
 
-#Initializations
+# Initializations
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks_GTX.dat')
-capture = cv.VideoCapture('filmik.mp4')
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks_GTX.dat")
+capture = cv.VideoCapture("video/ignore_video/filmik.mp4")
 
 # Finding landmark id for left and right eyes
 (leftEyeStart, leftEyeEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
@@ -26,8 +26,7 @@ def eye_aspect_ratio(eye):
     p2_minus_p6 = dist.euclidean(eye[1], eye[5])
     p3_minus_p5 = dist.euclidean(eye[2], eye[4])
     p1_minus_p4 = dist.euclidean(eye[0], eye[3])
-    ear = (p2_minus_p6 + p3_minus_p5) / (2.0 * p1_minus_p4)
-    return ear
+    return (p2_minus_p6 + p3_minus_p5) / (2.0 * p1_minus_p4)
 
 
 EYE_CLOSED_COUNTER = 0
@@ -64,23 +63,32 @@ while True:
                 eye_counter += 1
             EYE_CLOSED_COUNTER = 0
 
-        cv.putText(frame, "EAR: {}".format(round(ear, 2)), (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv.putText(
+            frame,
+            "EAR: {}".format(round(ear, 2)),
+            (10, 30),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 0, 255),
+            2,
+        )
 
-        # if EYE_CLOSED_COUNTER >= MAXIMUM_FRAME_COUNT:
-        cv.putText(frame, f"Count: {eye_counter}", (10, 50), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv.putText(
+            frame,
+            f"Count: {eye_counter}",
+            (10, 50),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 0, 255),
+            2,
+        )
         data.append(ear)
         timer.append(time.perf_counter() - start_time)
-        # dateTimeObj = datetime.now()
-        # timestampStr = dateTimeObj.strftime("(%H:%M:%S.%f)")
-        # time.append(timestampStr)
     cv.imshow("Frame", frame)
-    if cv.waitKey(20) & 0xFF == ord('d'):
+    if cv.waitKey(20) & 0xFF == ord("q"):
         data.pop(0)
         timer.pop(0)
         d = scipy.signal.medfilt(data, 3)
         plt.plot(timer, d)
-        step = len(timer) // 3  # liczba danych dzielona przez liczbę etykiet
-        # labels = [0, step, 2 * step, 3 * step]
-        # plt.xticks(labels)
         plt.show()
         break
